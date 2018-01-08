4ffe080060fa8b8c3cd139c05d9dbad61ae11edb
Sebastian Miele <sebastian.miele@gmail.com>
2018-01-08 06:53:07 +0100

    Fix typo

--- borg.el
+++ borg.el
@@ -54,7 +54,7 @@
    (directory-file-name
     (file-name-directory
      (or load-file-name buffer-file-name))))
-  "Directory beneath with drone submodules are placed.")
+  "Directory beneath which drone submodules are placed.")
 
 (defconst borg-user-emacs-directory
   (file-name-directory (directory-file-name borg-drone-directory))
