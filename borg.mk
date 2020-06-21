a013a0eee79f55be4e21261b4bf28061ed28de90
Jonas Bernoulli <jonas@bernoul.li>
2020-06-21 16:51:45 +0200

    make: Suppress another annoying warning
    As far as I am concerned prefixes that are only two characters long
    are perfectly okay.  This used to warn about the "ht" prefix of the
    "ht" package.

--- borg.mk
+++ borg.mk
@@ -18,6 +18,7 @@ SILENCIO += --eval "(fset 'original-message (symbol-function 'message))"
 SILENCIO += --eval "(fset 'message\
 (lambda (format &rest args)\
   (unless (or (equal format \"pcase-memoize: equal first branch, yet different\")\
+              (equal format \"Not registering prefix \\\"%s\\\" from %s.  Affects: %S\")\
               (and (stringp (car args))\
                    (string-match-p \"Scraping files for\" (car args))))\
     (apply 'original-message format args))))"
