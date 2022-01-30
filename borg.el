ff9de30d1a105baa627d8fc7565bda6598efc001
Jonas Bernoulli <jonas@bernoul.li>
2022-01-30 20:18:04 +0100

    Hardcode version string for now

--- borg.el
+++ borg.el
@@ -6,7 +6,7 @@
 ;; Homepage: https://github.com/emacscollective/borg
 ;; Keywords: tools
 
-;; Package-Version: 3.2.0
+;; Package-Version: 3.3.0-git
 ;; Package-Requires: ((emacs "26") (epkg "3.3.0") (magit "3.0.0"))
 ;;
 ;;   Borg itself does no actually require Emacs 26 and has no
