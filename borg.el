dfc5ca14c60797cd715065585d06d2b5744fbcc4
Jonas Bernoulli <jonas@bernoul.li>
2024-04-15 17:34:04 +0200

    Update contact information

--- borg.el
+++ borg.el
@@ -2,7 +2,7 @@
 
 ;; Copyright (C) 2016-2024 Jonas Bernoulli
 
-;; Author: Jonas Bernoulli <jonas@bernoul.li>
+;; Author: Jonas Bernoulli <emacs.borg@jonas.bernoulli.dev>
 ;; Homepage: https://github.com/emacscollective/borg
 ;; Keywords: tools
 
