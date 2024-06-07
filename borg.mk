8fb0bd8e1ab533a2ec3040d3fb2e58b0ad2d5724
Jonas Bernoulli <jonas@bernoul.li>
2024-06-07 21:49:24 +0200

    borg.mk: Don't require subr-x
    The kludge added in [1: 45b352b5] needed that, but the rest of that
    is long gone.
    
    1: 2017-12-21 45b352b5d0842eb9caf2d44bc49d1ed5cb7afad4
       Emacsg: make: Silence bogus warnings on newer Emacsen

--- borg.mk
+++ borg.mk
@@ -30,8 +30,7 @@ EMACS_EXTRA ?=
         init-clean init-build bootstrap bootstrap-borg
 .FORCE:
 
-SILENCIO  = --load subr-x
-SILENCIO += --eval "(setq byte-compile-warnings '(not docstrings))"
+SILENCIO  = --eval "(setq byte-compile-warnings '(not docstrings))"
 SILENCIO += --eval "(progn (require 'gv) (put 'buffer-substring 'byte-obsolete-generalized-variable nil))"
 SILENCIO += --eval "(fset 'original-message (symbol-function 'message))"
 SILENCIO += --eval "(fset 'message\
