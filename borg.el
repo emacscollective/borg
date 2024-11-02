cd4bb2b4b670f671611c72227869197c918b8ea7
Jonas Bernoulli <jonas@bernoul.li>
2024-11-02 13:04:09 +0100

    Release version 4.1.1

--- borg.el
+++ borg.el
@@ -6,7 +6,7 @@
 ;; Homepage: https://github.com/emacscollective/borg
 ;; Keywords: tools
 
-;; Package-Version: 4.1.0
+;; Package-Version: 4.1.1
 ;; Package-Requires: (
 ;;     (emacs "27.1")
 ;;     (epkg "4.0.1")
