61f9e083b8d9f9ca196758729adce2c286841991
Jonas Bernoulli <jonas@bernoul.li>
2026-01-01 19:58:20 +0100

    Release version 4.4.0

--- borg.el
+++ borg.el
@@ -6,11 +6,11 @@
 ;; Homepage: https://github.com/emacscollective/borg
 ;; Keywords: tools
 
-;; Package-Version: 4.3.0
+;; Package-Version: 4.4.0
 ;; Package-Requires: (
 ;;     (emacs "28.1")
 ;;     (epkg   "4.1")
-;;     (magit  "4.4"))
+;;     (magit  "4.5"))
 
 ;; SPDX-License-Identifier: GPL-3.0-or-later
 
