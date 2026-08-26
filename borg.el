c6118f3779659ed2ff8ec8f651bd66c8eea3d2bf
Jonas Bernoulli <jonas@bernoul.li>
2026-08-26 20:30:04 +0200

    Update library commentary

--- borg.el
+++ borg.el
@@ -35,13 +35,10 @@
 ;; The Borg assimilate Emacs packages as Git submodules.  Borg is
 ;; an alternative, bare-bones package manager for Emacs packages.
 
-;; Please consult the manual for more information:
-;; https://www.emacsmirror.net/manual/borg.
+;; Borg can be used by itself or alongside `package.el'.
 
-;; Borg can be used by itself or alongside `package.el'.  In the
-;; latter case Borg itself should be installed from Melpa, which
-;; is still experimental and not yet covered in the manual.  See
-;; https://github.com/emacscollective/borg/issues/46 for now.
+;; Please consult the manual, https://docs.emacsmirror.org/borg/,
+;; for more information:
 
 ;;; Code:
 
